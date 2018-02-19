d94142abbe2433ffeca56e7ac136dcd6602cd6e1
Clas Olaf Steinbru Andersen <coanders@stud.ntnu.no>
2018-02-19 06:17:14 +0100

    fixed tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -22,6 +22,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to tdt4242-exercise2-group!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to tdt4242-exercise2-group8!');
   }));
 });
