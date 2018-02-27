2cdf2624c89f62f0aa351646ccab06132fafa749
Clas Olaf Steinbru Andersen <coanders@stud.ntnu.no>
2018-02-27 16:39:53 +0100

    updated database URI to use heroku ENV

--- server/server.js
+++ server/server.js
@@ -6,7 +6,8 @@ const mongoose = require('mongoose');
 const server = express();
 
 // Database
-const db = mongoose.connect('mongodb://heroku_3msqfcw6:t562gfc8t8s3vqkpq1qj3t2c52@ds145438.mlab.com:45438/heroku_3msqfcw6');
+const dbURI = process.env.MONGODB_URI || 'mongodb://heroku_3msqfcw6:t562gfc8t8s3vqkpq1qj3t2c52@ds145438.mlab.com:45438/heroku_3msqfcw6'
+const mongodb = mongoose.connect(dbURI);
 const port = process.env.PORT || 4300
 
 // Routers
