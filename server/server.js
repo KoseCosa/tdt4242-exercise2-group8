9e98adb98bdcc16e936abb8b67922af47d163198
ingvild <ingvlgr@student.hist.no>
2018-02-27 11:49:55 +0100

    Server.js

--- server/server.js
+++ server/server.js
@@ -4,4 +4,9 @@ const server = express()
 const port = process.env.PORT || 4300
 
 server.use(express.static(__dirname + '/../dist'))
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'dist/index.html'));
+  });
+
 server.listen(port, () => console.log('Server listening on port '+port+'!'))
