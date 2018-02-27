c465baf95f1544c2e23e8a9aaa14a8c8038bd7d1
Clas Olaf Steinbru Andersen <coanders@stud.ntnu.no>
2018-02-27 16:36:20 +0100

    created mvp product db api

--- server/server.js
+++ server/server.js
@@ -1,11 +1,20 @@
+// Imports
 const express = require('express');
-const server = express();
-const apiRouter = require('./routes/api.js');
+const mongoose = require('mongoose');
 
+// Initialize Express Server
+const server = express();
+
+// Database
+const db = mongoose.connect('mongodb://heroku_3msqfcw6:t562gfc8t8s3vqkpq1qj3t2c52@ds145438.mlab.com:45438/heroku_3msqfcw6');
 const port = process.env.PORT || 4300
 
-server.use(express.static(__dirname + '/../dist'));
+// Routers
+const apiRouter = require('./routes/api.js');
+
+// Other Routes
+server.use(express.static(__dirname + '/../dist')); // Static Folder
 
 server.use('/api',apiRouter);
-
+// Start Listening
 server.listen(port, () => console.log('Server listening on port '+port+'!'));
