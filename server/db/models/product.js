70f892b8568cac0ed1e65bdbaee5ba391b74b461
Clas Olaf Steinbru Andersen <coanders@stud.ntnu.no>
2018-03-04 14:39:35 +0100

    update product model

--- server/db/models/product.js
+++ server/db/models/product.js
@@ -1,12 +1,15 @@
 mongoose = require('mongoose');
 
 const ProductSchema = mongoose.Schema({
-  name: String,
-  price: Number,
-  category: String,
-  stock: Number,
-  salePercentage: Number
-  // Add Package Deal Object
+  name: String;
+  price: Number;
+  category: String;
+  stock: Number;
+  salePercentage: { type: Number, min: 0, max: 100 };
+  packageDeal: {
+    get: Number;
+    pay: Number;
+  };
 });
 
 module.exports = mongoose.model('Product', ProductSchema);
