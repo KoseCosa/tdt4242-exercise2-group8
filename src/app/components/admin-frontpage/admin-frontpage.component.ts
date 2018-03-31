b997c8f6db016f31e65eaaf783bc521e93810918
Clas Olaf Steinbru Andersen <coanders@stud.ntnu.no>
2018-03-31 17:29:26 +0200

    added some simple styling

---
+++ src/app/components/admin-frontpage/admin-frontpage.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-frontpage',
+  templateUrl: './admin-frontpage.component.html',
+  styleUrls: ['./admin-frontpage.component.css']
+})
+export class AdminFrontpageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
